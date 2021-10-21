15498091deab04fc1f0f6e94af51d953e9274709
Paulo R. Marinho <pauloricmarinho@gmail.com>
2021-10-20 22:06:58 -0300

    PyTest Exemplo

--- mock_model.py
+++ mock_model.py
@@ -53,4 +53,3 @@ if __name__ == '__main__':
     #consulta_produto()
     insere_usuario('prmarinho', '01234')
     consulta_todos_usuarios()
-    test_send_http()
--- test_unitario.py
+++ test_unitario.py
@@ -1,5 +1,9 @@
 import pytest
 
 @pytest.mark.slow
+def test_vs():
+    assert True
+
+@pytest.mark.slow
 def test_send_http():
     assert True
